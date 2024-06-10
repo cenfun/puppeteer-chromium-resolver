4de7be15beb1d4dffbf5c927ace1ef4e4ece9888
cenfun <cenfun@gmail.com>
2024-06-10 23:39:09 +0800

    update deps

---
+++ eslint.config.js
@@ -0,0 +1,8 @@
+// https://eslint.org/docs/rules/
+
+const plus = require('eslint-config-plus');
+
+// https://eslint.org/docs/latest/use/configure/configuration-files
+module.exports = [
+    plus
+];
